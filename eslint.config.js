e67ade93c1f70d0757834ac64a0774c29c49707e
kizeu <116279616+kimzeuner@users.noreply.github.com>
2026-06-25 09:33:10 +0200

    Add ESLint configuration for JavaScript files

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["src/**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        HTMLElement: "readonly",
+        customElements: "readonly",
+        document: "readonly",
+        window: "readonly",
+        CustomEvent: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-console": "warn"
+    }
+  }
+];
