65b546e2bb09942475e819f767e4d333bed9d7eb
kizeu <116279616+kimzeuner@users.noreply.github.com>
2026-08-07 08:27:40 +0200

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,7 @@
 export const DEFAULT_CONFIG = {
   entity: "",
+  title: "",
+  header_layout: "inline",
   map_entity: "",
   battery_entity: "",
   charging_entity: "",
@@ -12,15 +14,18 @@ export const DEFAULT_CONFIG = {
   show_battery_header: true,
   show_status: true,
   action_layout: "icon",
+  
   map_rotation: 0,
   map_zoom: 1,
   map_offset_x: 0,
   map_offset_y: 0,
+  
   theme_button_background: "var(--card-background-color)",
   theme_primary_color: "var(--primary-color)",
   theme_warning_color: "var(--warning-color)",
   theme_error_color: "var(--error-color)",
   theme_border_color: "rgba(0,150,136,0.2)",
+  
   map_tap_action: { action: "more-info" },
   map_double_tap_action: { action: "none" },
   map_hold_action: { action: "none" },
